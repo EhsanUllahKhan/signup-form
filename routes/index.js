2e4e76f2286eeae9ea10abed0696ab0ad0ec82a1
EhsanUllahKhan <ullahkhanihsan@yahoo.com>
2020-10-20 14:49:13 +0500

    reqres api integration with expreass mean subscription

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,58 @@
 var express = require('express');
 var router = express();
+const request = require('request');
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+
+/* GET home page. */
+router.get('/user', function (req, res, next) {
+  request('https://reqres.in/api/users?page=2', function (error, response, body) {
+    var data = JSON.parse(body)
+    console.log(data["data"])
+    res.render("data", {
+      data: JSON.stringify(data["data"])
+    });
+  });
+});
+
+router.get("/user/:id", (req, res) => {
+  const accountId = Number(req.params.id);
+  //console.log(accountId)
+  var url = 'https://reqres.in/api/users/' + accountId;
+  request(url, function (error, response, body) {
+    var data = JSON.parse(body)
+    console.log(data["data"])
+    res.render("data", {
+      data: JSON.stringify(data["data"])
+    });
+  });
+})
+
+router.get("/user/unknown/:id", (req, res) => {
+  const accountId = Number(req.params.id);
+  //console.log(accountId)
+  var url = "https://reqres.in/api/unknown/" + accountId;
+  request(url, function (error, response, body) {
+    var data = JSON.parse(body)
+    console.log(data)
+    res.render("data", {
+      data: JSON.stringify(data)
+    });
+  });
+})
+
+router.get('/user/unknown', function (req, res, next) {
+  request('https://reqres.in/api/unknown', function (error, response, body) {
+    var data = JSON.parse(body)
+    console.log(data["data"])
+    res.render("data", {
+      data: JSON.stringify(data["data"])
+    });
+  });
 });
 
 router.post("/subscribe", function (req, res, next) {
@@ -13,8 +62,7 @@ router.post("/subscribe", function (req, res, next) {
   // 2. Subscribe the user to the mailing list
   // 3. Send a confirmation email
 
-  res.render("ehsan", {
-    information: "Capregsoft",
+  res.render("subscribed", {
     name: "Ehsan Ullah Khan",
     email
   });
